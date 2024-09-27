f0131f77bd32feccce249197cabd31928b5fc59c
Abi Kirkham <abbeykirkham9@icloud.com>
2024-09-27 14:01:25 +0000

    Add home app and templates

--- girls_get_tattoos/girls_get_tattoos/settings.py
+++ girls_get_tattoos/girls_get_tattoos/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -63,7 +64,10 @@ ROOT_URLCONF = 'girls_get_tattoos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [ 
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
