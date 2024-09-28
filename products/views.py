84df35014209aedefca24af6743d944c2ddc4ef7
Abi Kirkham <abbeykirkham9@icloud.com>
2024-09-28 11:33:09 +0000

    Add product detail page with view. Add media query to css

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from .models import Product
 
@@ -13,3 +14,14 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product-detail.html', context)
