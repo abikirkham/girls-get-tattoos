d0feba76edf6a5d573ab67e79dc61357822fb297
Abi Kirkham <abbeykirkham9@icloud.com>
2024-10-26 13:46:16 +0000

    Re-install crispy forms as since changing djang versions, unable to locate checkout.html - now resolved

--- girls_get_tattoos/settings.py
+++ girls_get_tattoos/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
     # Other
     'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -70,7 +71,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'girls_get_tattoos.urls'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 TEMPLATES = [
     {
