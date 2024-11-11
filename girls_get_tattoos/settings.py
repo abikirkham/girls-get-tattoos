552964e3ecbee45e6391ec0315ee4e9eb189016e
Abi Kirkham <abbeykirkham9@icloud.com>
2024-11-11 17:48:29 +0000

    Update env with keys for deployment with heroku

--- girls_get_tattoos/settings.py
+++ girls_get_tattoos/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '(16++&w=5l#9eyoqj(_yawuwgdxpn6g#f9q3apg-r^po$65yw&')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
