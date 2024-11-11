c20a58da68c248be0c546a7c4fb4521e37fce19f
Abi Kirkham <abbeykirkham9@icloud.com>
2024-11-11 16:23:35 +0000

    Update secret key

--- girls_get_tattoos/settings.py
+++ girls_get_tattoos/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^tn)-o4y^uj635my4l$lrmg8=pwgp)ve80+&75ehkks1zrc&0$'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
