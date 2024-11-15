61abcaf6a210eb8ec2c39f4990ab879de6fc545b
Abi Kirkham <abbeykirkham9@icloud.com>
2024-11-15 13:10:36 +0000

    Update app name in wsgi file for deployment

--- girls_get_tattoos/wsgi.py
+++ girls_get_tattoos/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'girls_get_tattoos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'girls-get-tattoos.settings')
 
 application = get_wsgi_application()
