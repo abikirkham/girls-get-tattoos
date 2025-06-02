bdd960ca2401a0e3247f2fe854ce9e015b1546a0
abikirkham <abbeykirkham9@icloud.com>
2025-06-02 18:58:05 +0100

    Fix import error: remove missing oauth2callback view

--- girls_get_tattoos/urls.py
+++ girls_get_tattoos/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import handler404
-from consultations.views import oauth2callback
 
 
 urlpatterns = [
